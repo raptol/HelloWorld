8b60267e7fdb2564067e3c8311a84f68ce568483
Tanishbansal2011 <72216510+Tanishbansal2011@users.noreply.github.com>
2020-10-01 22:37:39 +0530

    Update helloworld.py
    Please don't reject this pull request if you don't want to merge the request , it is totally fine but please don't reject this pull request.

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
-print("Hello World3")
+a = int(input('''Choose 1 to write "Hello World" 
+Choose 2 to write "Hello Sparky" '''))print("Hello World3")
+
+if a = 1 
+  print("Hello World")
+  
 print("헬로우 월드3")
